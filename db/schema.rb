cbb5277cbe025300d0cb4c18a6c02d95a2a283a6
Stefano <snebo54@hotmail.com>
2024-05-07 14:57:09 +0100

    add associtaion between user and evetns

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_07_133634) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_07_135402) do
   create_table "events", force: :cascade do |t|
     t.string "title"
     t.date "starting"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_events_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -32,4 +34,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_07_133634) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "events", "users"
 end
