6e3dbf65799b8f98fae4a47ee4822e15ccbe3c1d
Stefano <snebo54@hotmail.com>
2024-05-07 22:45:32 +0100

    create method to add events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,4 +3,22 @@ class EventsController < ApplicationController
   def index
     @events = Event.all
   end
+  def new
+    # @user = User.find(current_user.id)
+    @event = current_user.events.build
+  end
+  def create
+    @event = current_user.events.build(event_params)
+    if @event.save
+      redirect_to user_posts_path, notice: "Created event!"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:title, :description, :starting)
+  end
 end
