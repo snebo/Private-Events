05cc16989ffbea0ba8879d43fb7563fdfe088d7a
Stefano <snebo54@hotmail.com>
2024-05-10 20:28:15 +0100

    create button method to follow event

--- app/controllers/attendees_controller.rb
+++ app/controllers/attendees_controller.rb
@@ -1,7 +1,7 @@
 class AttendeesController < ApplicationController
   def create
-    @event = Event.find(params[:id]) # get current event
-    @attendee = @event.attendees.build(attend_params)
+    @event = Event.find(params[:event_id]) # get current event
+    @attendee = @event.attendees.build(user_id: params[:user_id], event_id: params[:event_id])
     if @attendee.save
       flash[:success] = "#{current_user.username.capitalize} is now attending"
     else
@@ -9,16 +9,17 @@ class AttendeesController < ApplicationController
     end
     redirect_to @event # take me back to event after registering
   end
+
   def destroy
-    @attendee = Attendee.find(params[:id])
+    @attendee = Attendee.find_by_user_id(current_user.id)
     @attendee.destroy
     flash[:success] = "No longer following this event"
     redirect_to @event
   end
 
-  private
+  # private
 
-  def attend_params
-    params.require(:attendees).permit(:user_id, :event_id)
-  end
+  # def attend_params
+  #   params.require(:attendees).permit(:user_id, :event_id)
+  # end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   has_many :events
-  has_many :events, through: :attendees
+  has_many :attendees, through: :events
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatableS
 end
